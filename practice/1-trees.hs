36e74695a2c4c8f17393b3c83c2a6e8d8cccd8ec
Sherod Taylor <sherodtaylor@gmail.com>
2019-01-24 08:41:29 -0500

    tree problems

--- practice/1-trees.hs
+++ practice/1-trees.hs
@@ -3,6 +3,9 @@ module Tree where
               | FilledNode a (Tree a) (Tree a)
               deriving (Eq,Ord,Show,Read)
 
+  buildLeaf :: (Ord a, Num a) => a -> Tree a
+  buildLeaf x = FilledNode x EmptyNode EmptyNode
+
   heightOfTree :: (Ord a, Num a) => Tree a -> a
   heightOfTree EmptyNode = 0
   heightOfTree (FilledNode _ left right) =
@@ -10,14 +13,24 @@ module Tree where
         rightHeight = (heightOfTree right) + 1
     in max leftHeight rightHeight
 
-  inOrderArray :: a => Tree a -> a
-  inOrderArray EmptyNode = 0
-  inOrderArray (FilledNode _ left right) =
-    let leftVal  = inOrderArray left
-        rightHeight = (heightOfTree right) + 1
-    in max leftHeight rightHeight
+  inOrderArray :: (Ord a, Num a) => Tree a -> [a]
+  inOrderArray EmptyNode = []
+  inOrderArray (FilledNode a left right) =
+    let leftArr  = inOrderArray left
+        rightArr = inOrderArray right
+    in leftArr ++ [a] ++ rightArr
 
+  preOrderArray :: (Ord a, Num a) => Tree a -> [a]
+  preOrderArray EmptyNode = []
+  preOrderArray (FilledNode a left right) =
+    [a] ++ (preOrderArray left) ++ (preOrderArray right)
   
+  postOrderArray :: (Ord a, Num a) => Tree a -> [a]
+  postOrderArray EmptyNode = []
+  postOrderArray (FilledNode a left right) =
+    (postOrderArray left) ++ (postOrderArray right) ++ [a] 
+  
+
 
   
   
